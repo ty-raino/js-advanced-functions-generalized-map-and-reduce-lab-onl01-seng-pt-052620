7025898c5c6b6828bc0210f95bb1b521a0fcf372
github username <github email address>
2020-12-26 00:34:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(source, callbck) {
+   let arr = []
+
+   for (let i = 0; i < source.length; i++){
+       arr.push(callbck(source[i]))
+   }
+
+   return arr
+}
+
+
+function reduce(source, callbck, startingValue) {
+  let reducedValue = !!startingValue ? startingValue : source[0]
+  let int = !!startingValue ? 0 : 1
+
+  for (; int < source.length; int++) {
+    reducedValue = callbck(source[int], reducedValue)
+  }
+
+  return reducedValue;
+}
